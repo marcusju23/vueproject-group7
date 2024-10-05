c266cb9386954e3e5b6c2ccd2d507917c7a4ed70
Marcus <marcus.olsson98@gmail.com>
2024-10-05 14:17:45 +0200

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  /** Configure Tailwind to remove unused styles in production */
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // 'media' or 'class' can also be used
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
