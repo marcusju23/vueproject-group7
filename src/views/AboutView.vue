26f6ec91688e2e6fc49dbc62d99fe7874b840aba
Marcus <marcus.olsson98@gmail.com>
2024-10-07 14:38:31 +0200

    Add AboutView.vue

---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="container">
+    <div class="text-center">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Nunc feugiat rhoncus interdum. Ut volutpat euismod lacinia.
+      Pellentesque at dui vel nulla malesuada condimentum et eget justo.
+      Praesent massa mauris, sodales suscipit diam sit amet, malesuada cursus felis.
+      Ut fringilla ex urna, vel consectetur ante dictum sit amet.
+      Quisque tempor hendrerit sapien eu venenatis. Pellentesque ut iaculis eros.
+      Nulla eu dictum ex, ac fermentum ex.</p>
+    </div>
+  </main>
+</template>
