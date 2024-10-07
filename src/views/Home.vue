adbfb26c6c299f35fc614d61b8a93498997f0630
Danback <daniel.backberg@iths.se>
2024-10-07 12:00:12 +0200

    Building API services #1

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="product-list">
+    <ProductCard v-for="product in products" :key="product.id" :product="product"/>
+  </div>
+</template>
+
+<script>
+import { apiService } from '@/services/apiService.js';
+import ProductCard from '@/components/ProductCard.vue';
+
+export default {
+  components: { ProductCard },
+  data() {
+    return {
+      products: []
+    };
+  },
+  async mounted() {
+    this.products = await apiService.getProducts();
+  }
+};
+</script>
+
+<style>
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
+
+
+<!--Detta är vart vi hämntar produkter från APIn, sidan laddas och visar dem med Productcard filen-->
---
+++ src/views/ProductDetail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="product">
+    <h1>{{ product.title }}</h1>
+    <img :src="product.images[0]" alt="product image"/>
+    <p>{{ product.description }}</p>
+    <p>{{ product.price }} $</p>
+  </div>
+</template>
+
+<script>
+import { apiService } from '@/services/apiService.js';
+
+export default {
+  data() {
+    return {
+      product: null
+    };
+  },
+  async mounted() {
+    const id = this.$route.params.id;
+    this.product = await apiService.getProductById(id);
+  }
+};
+</script>
+
+<!--sida som visart detaljerna för varje produkt när användaren klickar på "View details"-->
