735ac0af1c98a9633275a9070ea8b023fe0efd21
Marcus <marcus.olsson98@gmail.com>
2024-10-07 14:37:10 +0200

    Add Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav>
+    <ul class="flex space-x-4 p-4 bg-gray-800 text-white">
+      <li class="relative">
+        <button @click="toggleDropdown" class="hover:text-gray-300 focus:outline-none">Menu</button>
+        <ul v-if="isDropdownOpen" class="absolute left-0 mt-2 bg-gray-800 text-white shadow-lg">
+          <li><RouterLink active-class="active" to="/" class="menu-item">Home</RouterLink></li>
+          <li><RouterLink active-class="active" to="/contact" class="menu-item">Contact</RouterLink></li>
+          <li><RouterLink active-class="active" to="/about" class="menu-item">About</RouterLink></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const isDropdownOpen = ref(false);
+
+function toggleDropdown() {
+  isDropdownOpen.value = !isDropdownOpen.value;
+}
+
+// Close dropdown when clicking outside
+document.addEventListener('click', (event) => {
+  const dropdownButton = document.querySelector('button');
+  const dropdownMenu = document.querySelector('ul.absolute');
+  if (!dropdownButton.contains(event.target) && dropdownMenu && !dropdownMenu.contains(event.target)) {
+    isDropdownOpen.value = false;
+  }
+});
+</script>
