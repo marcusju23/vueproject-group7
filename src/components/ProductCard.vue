9a7263fed4f439c61c36002e0fcc53c3b03eab63
Marcus <marcus.olsson98@gmail.com>
2024-10-08 18:04:33 +0200

    Add router link to the card, update styling classes and remove redundant word in alt

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="product-card">
-    <img :src="product.images[0]" alt="product image" class="product-image"/>
-    <h3>{{ product.title }}</h3>
-    <p>{{ product.price }} $</p>
-    <router-link :to="`/product/${product.id}`">View Details</router-link>
-  </div>
+  <router-link :to="`/product/${product.id}`">
+    <div class="product-card">
+      <img :src="product.images[0]" alt="product" class="product-image"/>
+      <h3 class="product-h3">{{ product.title }}</h3>
+      <p class="product-price">${{ product.price }}</p>
+    </div>
+  </router-link>
 </template>
 
 <script setup>
