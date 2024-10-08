a5af12c7607c78bfef42c39965adf86085696e90
Marcus <marcus.olsson98@gmail.com>
2024-10-08 18:05:44 +0200

    Update styling and remove redundant word in alt

--- src/views/ProductDetailView.vue
+++ src/views/ProductDetailView.vue
@@ -1,11 +1,19 @@
 <template>
-  <div v-if="product">
-    <h1>{{ product.title }}</h1>
-    <img :src="product.images?.[0]" alt="product image" v-if="product.images?.length" />
-    <p>{{ product.description }}</p>
-    <p>{{ product.price }} $</p>
+  <div class="flex items-center justify-center h-screen">
+    <div class="flex" v-if="product">
+      <img class="max-w-lg" :src="product.images?.[0]" alt="product" v-if="product.images?.length" />
+      <div class="product-details ml-4 relative">
+        <h1 class="product-h1">{{ product.title }}</h1>
+        <p class="product-p max-w-xs">{{ product.description }}</p>
+        <div class="absolute bottom-0 right-0 flex flex-col items-end">
+          <p class="product-price text-lg">${{ product.price }}</p>
+          <button class="bg-neutral-800 text-white py-2 px-4 mt-2 rounded hover:bg-stone-600">Add to cart</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref, onMounted } from 'vue';
