4bcf7711dcd58d8f0213c38af44b46bed4ec06e2
Marcus <marcus.olsson98@gmail.com>
2024-10-09 22:03:57 +0200

    Fix height adjustment and spacing

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,9 +1,11 @@
 <template>
   <RouterLink :to="`/product/${product.id}`">
-    <div class="product-card">
-      <img class="product-image" :src="product.image" alt="product"/>
-      <h3 class="product-title">{{ product.title }}</h3>
-      <p class="product-price">${{ product.price }}</p>
+    <div class="product-card h-full flex flex-col justify-between">
+      <img class="product-image" :src="product.image" alt="product" />
+      <h3 class="product-title line-clamp-2 h-10 m2">{{ product.title }}</h3>
+      <div class="bottom-div flex-grow flex items-center justify-center">
+        <span class="product-price">${{ product.price }}</span>
+      </div>
     </div>
   </RouterLink>
 </template>
