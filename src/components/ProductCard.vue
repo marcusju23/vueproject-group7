5e44e93620a51d6ded4705f2e1e37ea9bd54028e
Marcus <marcus.olsson98@gmail.com>
2024-10-09 17:35:08 +0200

    Remove description

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,7 +3,6 @@
     <div class="product-card">
       <img class="product-image" :src="product.image" alt="product"/>
       <h3 class="product-title">{{ product.title }}</h3>
-      <p class="product-description">{{ product.description }}</p>
       <p class="product-price">${{ product.price }}</p>
     </div>
   </RouterLink>
