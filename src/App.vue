c1fa1cd146fca51061b1c50960ab3a00ff7a25f0
Marcus <marcus.olsson98@gmail.com>
2024-10-10 18:21:12 +0200

    Remove Footer component from App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
   <main>
-    <Navbar></Navbar>
+    <Navbar />
     <RouterView />
-    <Footer></Footer>
   </main>
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import {RouterView} from 'vue-router';
-import Footer from "@/components/Footer.vue";
 </script>
