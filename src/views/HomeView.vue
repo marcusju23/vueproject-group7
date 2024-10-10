bead2dae8686970a1e7606c93f937a1859ee7a89
Marcus <marcus.olsson98@gmail.com>
2024-10-10 17:20:18 +0200

    Fix HomeView to show categories with related products rather than just all products

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="product-list">
-    <ProductCard v-for="(product, index) in products" :key="index" :product="product"/>
+  <div class="container mx-auto">
+    <div class="container mt-8" v-for="(productsInCategory, category) in categoryProducts" :key="category">
+      <h3 class="category-title text-2xl">{{ category }}</h3>
+      <div class="flex flex-wrap">
+        <ProductCard v-for="(product, index) in productsInCategory" :key="index" :product="product" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,20 +14,36 @@ import { ref, onMounted } from 'vue';
 import ProductCard from '@/components/ProductCard.vue';
 import { apiService } from '@/api/apiService';
 
-
-const products = ref([]);
-
+const categoryProducts = ref({});
 
 onMounted(async () => {
+  const data = await fetchProducts();
+  categoryProducts.value = groupProductsByCategory(data);
+});
+
+const fetchProducts = async () => {
   try {
     const data = await apiService.getProducts();
     if (Array.isArray(data)) {
-      products.value = data;
+      return data;
     } else {
-      console.error('Unexpected data format:', data);
+      console.error("Unexpected data format:", data);
+      return [];
     }
   } catch (error) {
-    console.error('Error fetching products:', error);
+    console.error("Error fetching products:", error);
+    return [];
   }
-});
+};
+
+const groupProductsByCategory = (products) => {
+  return products.reduce((grouped, product) => {
+    const categoryName = product.category;
+    if (!grouped[categoryName]) {
+      grouped[categoryName] = [];
+    }
+    grouped[categoryName].push(product);
+    return grouped;
+  }, {});
+};
 </script>
