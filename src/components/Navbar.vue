40ac7d1632df8d86307e9aecbaabd10261a60c27
Danback <daniel.backberg@iths.se>
2024-10-10 16:08:20 +0200

    Added searchbar to navbar, currently not functional

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,43 @@
 <template>
-  <nav>
-    <ul class="flex space-x-4 p-4 bg-neutral-800 text-white">
-      <li class="relative">
-        <button @click="toggleDropdown">Menu</button>
-        <ul v-if="isDropdownOpen" class="absolute left-0 mt-2 bg-neutral-800 text-white shadow-lg">
-          <li><RouterLink active-class="active" to="/" class="menu-item">Home</RouterLink></li>
-          <li><RouterLink active-class="active" to="/contact" class="menu-item">Contact</RouterLink></li>
-          <li><RouterLink active-class="active" to="/about" class="menu-item">About</RouterLink></li>
-        </ul>
-      </li>
-    </ul>
+  <nav class="flex justify-between items-center p-4 bg-neutral-800 text-white">
+    <div class="flex items-center space-x-4">
+      <ul class="flex space-x-4">
+        <li class="relative">
+          <button @click="toggleDropdown" class="hover:text-gray-300 focus:outline-none">Menu</button>
+          <ul v-if="isDropdownOpen" class="absolute left-0 mt-2 bg-neutral-800 text-white shadow-lg">
+            <li><RouterLink active-class="active" to="/" class="menu-item">Home</RouterLink></li>
+            <li><RouterLink active-class="active" to="/contact" class="menu-item">Contact</RouterLink></li>
+            <li><RouterLink active-class="active" to="/about" class="menu-item">About</RouterLink></li>
+          </ul>
+        </li>
+      </ul>
+
+      <div class="relative">
+        <input
+            v-model="searchQuery"
+            @input="emitSearchQuery"
+            type="text"
+            placeholder="Search for products..."
+            class="w-64 px-4 py-2 rounded-lg bg-neutral-700 text-white placeholder-gray-400 focus:outline-none focus:ring focus:ring-green-500"
+        />
+      </div>
+    </div>
   </nav>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import { ref } from 'vue';
+import { defineEmits } from 'vue';
 
+const searchQuery = ref('');
 const isDropdownOpen = ref(false);
+const emit = defineEmits(['updateSearchQuery']);
+
+function emitSearchQuery() {
+  emit('updateSearchQuery', searchQuery.value);
+}
 
 function toggleDropdown() {
   isDropdownOpen.value = !isDropdownOpen.value;
 }
-
-// Close dropdown when clicking outside
-document.addEventListener('click', (event) => {
-  const dropdownButton = document.querySelector('button');
-  const dropdownMenu = document.querySelector('ul.absolute');
-  if (!dropdownButton.contains(event.target) && dropdownMenu && !dropdownMenu.contains(event.target)) {
-    isDropdownOpen.value = false;
-  }
-});
 </script>
