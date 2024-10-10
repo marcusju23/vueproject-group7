752520f83d4cc868a70daa329de42c483f4c7909
Marcus <marcus.olsson98@gmail.com>
2024-10-10 18:21:47 +0200

    Add Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,20 +1,22 @@
 <template>
-    <footer class="primary-footer">
-        <div class="container">
-            <p>{{ footerText }}</p>
-        </div>
-    </footer>
-
+  <footer class="bg-neutral-800 mt-8">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-white sm:text-center">
+        <img class="inline w-6 h-6 mx-2" src="@/components/icons/github-mark-white.png" alt="GitHub Icon"/>
+        <a href="https://github.com/marcusju23/vueproject-group7" class="hover:underline">vueproject-group7</a>
+      </span>
+      <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0">
+        <RouterLink to="/about">
+          <li>
+            <p class="hover:underline me-4 md:me-6">About</p>
+          </li>
+        </RouterLink>
+        <RouterLink to="/contact">
+          <li>
+            <p class="hover:underline">Contact</p>
+          </li>
+        </RouterLink>
+      </ul>
+    </div>
+  </footer>
 </template>
-
-<script>
-    
-    export default {
-        data(){
-            return{
-                footerText: 'Vueproject inc.'
-            }
-            
-        }
-    }
-</script>
