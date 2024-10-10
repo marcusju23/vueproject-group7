82255cfa0da882d2f1154608ea516be380849117
Marcus <marcus.olsson98@gmail.com>
2024-10-11 00:13:55 +0200

    Revert "fixed footer routing"
    This reverts commit 3182d111d60d1c04e6c0d8e38d5b373a9d17f371.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <header>
-    <Navbar />
-  </header>
   <main>
+    <Navbar />
     <RouterView />
   </main>
-  <footer>
-    <Footer />
-  </footer>
 </template>
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
 import {RouterView} from 'vue-router';
-import Footer from "@/components/Footer.vue";
 </script>
