53b7c112cfbe08eba8e9d902071b3f166bdb0169
Danback <daniel.backberg@iths.se>
2024-10-22 13:16:22 +0200

    Products are now smaller and checkout button is centered with better styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,7 @@
   <nav class="z-50 sticky top-0 flex justify-between items-center p-4 backdrop-blur-md bg-black/25 text-white">
     <div class="flex items-center space-x-4">
       <ul class="flex space-x-4">
-        <RouterLink active-class="active" to="/"><img class="max-h-7" src="@/components/icons/home-icon.png" alt="Home">
-        </RouterLink>
+        <RouterLink active-class="active" to="/"><img class="max-h-7" src="@/components/icons/home-icon.png" alt="Home"></RouterLink>
         <li class="relative">
           <button @click="toggleDropdown" class="hover:text-gray-300 focus:outline-none">
             <img class="max-h-7" src="@/components/icons/hamburger.png" alt="Menu">
@@ -27,7 +26,7 @@
             class="absolute mt-2 w-96 bg-white rounded-lg shadow-lg max-h-48 overflow-y-auto z-10">
           <li v-for="product in filteredResults" :key="product.id" @click="goToProduct(product.id)"
               class="px-4 py-2 flex items-center cursor-pointer hover:bg-gray-100 text-black">
-            <img :src="product.image" alt="Product" class="w-12 h-12 object-cover mr-4 rounded"/>
+            <img :src="product.image" alt="Product" class="w-12 h-12 object-cover mr-4 rounded" />
             <div class="flex-1">
               <p class="text-gray-900">{{ product.title }}</p>
               <p class="text-gray-600 text-sm">${{ product.price }}</p>
@@ -39,36 +38,34 @@
 
     <div class="flex items-right cart-container">
       <div class="relative">
-        <button @click="toggleCart" class="hover:text-gray-300 focus:outline-none cart-btn">
+        <button @click="toggleCart" class="hover:text-gray-300 focus:outline-none cart-btn" ref="cartButton">
           <img class="max-h-9" src="@/components/icons/shopping-cart.png" alt="Cart">
-          <span v-if="cartStore.cartItemCount()"
-                class="absolute top-0 right-0 bg-red-500 text-white rounded-full w-6 h-6 text-center">
+          <span v-if="cartStore.cartItemCount()" class="absolute top-0 right-0 bg-red-500 text-white rounded-full w-6 h-6 text-center">
             {{ cartStore.cartItemCount() }}
           </span>
         </button>
 
-        <div v-if="isCartOpen" ref="cartDropdown"
-             class="z-50 absolute right-[-45%] top-14 w-[400px] text-white backdrop-blur-lg bg-black/30">
+        <div v-if="isCartOpen" ref="cartDropdown" class="z-50 absolute right-[-45%] top-14 w-[400px] text-white backdrop-blur-lg bg-black/30">
           <div class="max-h-[300px] overflow-y-auto">
             <ul v-for="(cartProduct, index) in cartStore.products" :key="index">
               <li>
                 <div class="flex items-center">
-                  <img class="p-4 max-h-24" :src="cartProduct.image" :alt="cartProduct.title">
-                  <div class="p-4 grow">
-                    <p class="text-xl">{{ cartProduct.title }}</p>
-                    <p class="text-xl bold">${{ cartProduct.price }}</p>
-                    <p>Quantity: {{ cartProduct.quantity }}</p>
+                  <img class="p-2 max-h-16" :src="cartProduct.image" :alt="cartProduct.title">
+                  <div class="p-2 grow">
+                    <p class="text-lg">{{ cartProduct.title }}</p>
+                    <p class="text-md bold">${{ cartProduct.price }}</p>
+                    <p class="text-sm">Quantity: {{ cartProduct.quantity }}</p>
                   </div>
-                  <button @click="removeItem(index, $event)" class="text-white bg-black/30 p-2 m-2 rounded">X</button>
+                  <button @click="removeItem(index, $event)" class="text-white bg-black/30 p-1 m-1 rounded">X</button>
                 </div>
               </li>
             </ul>
           </div>
-          <div class="flex justify-center content-center flex-col">
-            <p class="text-white p-4 text-2xl">Total Price : ${{ totalCartPrice }}</p>
-            <div>
-              <button class="text-white w-100 p-4 backdrop-blur-md bg-black/30">Checkout</button>
-            </div>
+
+          <div class="p-4">
+            <button class="text-white w-full py-3 bg-green-500 rounded hover:bg-green-600 transition">
+              Checkout
+            </button>
           </div>
         </div>
       </div>
